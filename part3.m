43e67adc91748f6199f65b82724fc9a9551d7b44
matheogodenzi <matheo.godenzi@epfl.ch>
2022-10-05 12:00:56 +0200

    added all the parts and instructions of the project

---
+++ part3.m
@@ -0,0 +1,22 @@
+% -------------------------------------------------------------------------
+% Course: Hydrology for Engineers
+% Assignment 1
+% Part 3: Construction of depth-duration-frequency (DDF) curves 
+% -------------------------------------------------------------------------
+
+clear variables %clear the workspace variables
+close all %close alla figures
+clc %clear the command window
+
+% -------------------------------------------------------------------------
+% # 1: Calibrate the DDF curve parameters
+% useful functions: linspace
+% -------------------------------------------------------------------------
+
+% import the data from previous part
+load assignment1_output_part2.mat 
+
+
+
+
+
