09dd607c641a8839f3e61fa1f4f1ef54ec304324
Mikael <mikael.gonzalez@epfl.ch>
2022-10-11 18:50:27 +0200

    advancements in part 3,

--- part3.m
+++ part3.m
@@ -21,15 +21,51 @@ load assignment1_output_part2.mat
 % finding them by brute force, i.e. testing all possibilities and keeping
 % the ones that matches the best
 
-for cspace = linspace(0,100,150)
-    for fspace = linspace(-1, 1, 200)
-        for espace = linspace(0, 1, 200)
-        
-        
-        
+% we compare computed h with estimated h by gumbel curves
+
+D = [1,3,6,12,24,48];
+% output is 3 matrices of parameters, one for each return period
+% for each return period, we have 3 parameters per event duration
+paramT10 = zeros(3,6); % rows are parameters, columns are durations
+paramT40 = zeros(3,6); % rows: c, e, f
+paramT100 = zeros(3,6);
+
+thres = [25,60,120];
+for k = 1:3 % iterating over return periods
+    min_diff = thres(k);
+    for l = 1:6 % iterating over event durations
+        for cspace = linspace(0,100,150)
+            for fspace = linspace(-1, 1, 200)
+                for espace = linspace(0, 1, 200)
+                    hcomp = cspace*D(l)/(D(l).^espace + fspace); 
+                    hgum = H_Gum(k,l);
+                    sqerr = (hcomp - hgum).^2;
+                    if sqerr < min_diff
+                        min_diff = sqerr;
+                        if k == 1
+                            paramT10(1, l) = cspace;
+                            paramT10(2, l) = espace;
+                            paramT10(3, l) = fspace;
+                        end
+                        if k == 2
+                            paramT40(1, l) = cspace;
+                            paramT40(2, l) = espace;
+                            paramT40(3, l) = fspace;
+                        end
+                        if k == 3
+                            paramT100(1, l) = cspace;
+                            paramT100(2, l) = espace;
+                            paramT100(3, l) = fspace;
+                        end
+                   
+                    end
+
+                end
+            end
         end
     end
 end
 
-
-
+%% wbpa
+paramT10
+paramT40
