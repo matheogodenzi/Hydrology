4b1371f6211b43074fc259561e34bfb30945d6ca
matheogodenzi <matheo.godenzi@epfl.ch>
2022-11-20 03:43:20 +0100

    graphs

---
+++ Assignement 2/Assignment2_material/CN_method_math.m
@@ -0,0 +1,171 @@
+% Specific goal: Compute effective precipitation Je and infiltration I intensity  
+% for three different rainfall events using the Curve Number method
+
+
+% beginning of the script
+
+clear variables %clear the workspace variables
+close all %close all figures
+clc %clear the command window
+
+%%
+% --------------------------------------------------------------------------
+% 1) Data import
+%--------------------------------------------------------------------------
+
+
+% importing the data
+
+load('event_1.mat')
+load('event_2.mat')
+
+% for T = 100 our DDF had the following parameters: 
+c = 40.2685;
+e = 0.7035;
+f = -0.0452;
+
+h_4h = c*4/(4.^e+ f);
+
+event_3 = zeros(4,1);
+for k = 1:4
+    event_3(k)=h_4h/4;
+end 
+
+event_3;
+
+% creating a matrix 
+event_matrix = [event_1(:), event_2(:), event_3(:)];
+
+
+%%
+% --------------------------------------------------------------------------
+% 2) Compute the average CN of the catchment
+%--------------------------------------------------------------------------
+load('../Parameters/SCSpars.mat')
+percentages = reshape(percentages,[1,3]);
+Average_CN = sum(CN.*percentages)/sum(percentages);
+
+
+%%
+% --------------------------------------------------------------------------
+% 3) Implement the CN method
+%--------------------------------------------------------------------------
+
+% Potential maximum soil moisture retention S
+S = 25.4*(1000/Average_CN-10);
+
+%SETTING VARIABLE VECTORS FOR EACH PARAMETERS 
+% The cumulative precipitation (P [mm])
+P = zeros(4,3);
+% The initial abstraction (Ia [mm])
+Ia = zeros(1,3);
+% The cumulative infiltration (Fa [mm]);
+Fa = zeros(4,3);
+% The cumulative effective precipitation (Pe [mm]);
+Pe = zeros(4,3);
+% The infiltration intensity (I [mm/h]);
+I = zeros(4,3);
+% The effective rainfall intensity (Je [mm/h]);
+Je = zeros(4,3);
+
+for l = 1:3
+    Ia(l)=0.2*S;
+    for k = 1:4
+        % time step of an hour so we multiply each rainfall intensity by 1 and
+        % add it to the previous to get the cumulative rainfall after n hours
+        if k == 1
+            P(k,l)= event_matrix(k,l);
+        else 
+            P(k,l)= P(k-1, l) + event_matrix(k,l);
+        end 
+        
+        % computing Pe
+        if P(k,l) <= Ia(l)
+            Pe(k,l)=0;
+        else 
+            Pe(k,l) = ((P(k,l)-Ia(l))^2)/(P(k,l)-Ia(l)+S);
+        end 
+
+        % computing Fa
+        Fa(k,l)= (S*Pe(k,l))/(P(k,l)-Ia(l));
+
+        % computing Je and I since the time step in 1 we divide by 1
+        if k == 1
+            Je(k,l) = Pe(k,l);
+            I(k,l) = Fa(k,l);
+        else
+            Je(k,l)= (Pe(k,l)-Pe(k-1,l))/1;
+            I(k,l)= (Fa(k,l)-Fa(k-1,l))/1;
+        end 
+    end 
+end 
+
+
+%%
+% --------------------------------------------------------------------------
+% 4) Show rainfall separation in a figure
+%--------------------------------------------------------------------------
+
+% Make a figure that includes, for each event: the rainfall intensity J [mm/h],
+% the infiltration intensity I [mm/h] and the effective precipitation intensity 
+% Je [mm/h]. Please use the same limits for the y-axis in the three events. 
+% You can either use one figure and three subplots or three different figures 
+% for the three events. Line plots with markers or bar plots are equally fine.
+t = 1:4;
+figure(1)
+
+for m = 1:3
+    subplot(3,1,m)
+    plot(t, event_matrix(:,m), '-o');
+    hold on
+    plot(t, I(:,m), '-o');
+    hold on 
+    plot(t, Je(:,m), '-o');
+    hold off
+    xlabel('time [h]')
+    ylabel('intensity [mm/h]')
+    title('event ' + string(m))
+    legend("J",  "I",  "Je");
+end 
+
+%{
+subplot(3,1,2)
+plot(t, event_matrix(:,2));
+hold on
+plot(t, I(:,2));
+hold on 
+plot(t, Je(:,2));
+hold off
+xlabel('time [h]')
+ylabel('intensity [mm/h]')
+title('event 2')
+legend("J",  "I",  "Je");
+
+subplot(3,1,3)
+plot(t, event_matrix(:,3));
+hold on
+plot(t, I(:,3));
+hold on 
+plot(t, Je(:,3));
+hold off
+xlabel('time [h]')
+ylabel('intensity [mm/h]')
+title('event 3')
+legend("J",  "I",  "Je");
+
+
+subplot(3,1,)
+plot(,GR);
+ylabel('Global radiation')
+title('first event')
+
+subplot(3,1,2)
+plot(t,AT);
+ylabel('air temperature')
+title('second event')
+
+subplot(3,1,3)
+plot(t,RAH);
+ylabel('relative air humidity')
+title('third event')
+%}
