831a8da647bdc24ad97cd5e067aab9df345fabc1
matheogodenzi <matheo.godenzi@epfl.ch>
2022-11-29 20:09:56 +0100

    le gros est là pour la partie deux, il faut encore revoir les graphiques finaux

--- Assignement 2/Assignment2_material/part_2_math.m
+++ Assignement 2/Assignment2_material/part_2_math.m
@@ -8,12 +8,13 @@ clc %clear the command window
 load('output_part1.mat');
 %% (1)
 
-n_sub = 3; %each hour is broken into n_int intervals
+n_sub = 4; %each hour is broken into n_int intervals
 dt = 1/n_sub; % timestep [h]
 
-t_Je= linspace(0,4-1,4);
-t_Jedt = linspace(0,4-dt,4/dt);
-Jedt = interp1(t_Je, Je, t_Jedt ,'previous','extrap');
+t_Je= linspace(0,4-1,4); % precipitation time 
+%t_Jedt = linspace(0,4-dt,4/dt); % precipitation time for dt timesteps starting at zero (first version)
+t_Jedt = dt*(dt:4/dt); % precipitation time for dt timesteps starting at dt (second version)
+Jedt = interp1(t_Je, Je, t_Jedt ,'previous','extrap'); % effective precipitation extrapolation
 
 %% (2) Watershed IUH
 % loading IUH params
@@ -21,41 +22,44 @@ load('../Parameters/IUHpars.mat')
 % generating watershed IUH gamma distribution
 %gammaIUH = zeros(size(Jedt));
 
-% trying on a finite time interval
-T = linspace(0,1000, 1000/dt);
+% defining IUH time interval 
+cutoff = 70;
+%T = linspace(0,1000, 1000/dt); % first version
+t_iuh=(dt:dt:cutoff); %second version 
 
-IUHW = gampdf(T , par_shape, par_scale);
+%IUHW = gampdf(T , par_shape, par_scale);% first version --> less precise 
+IUHW = gampdf(t_iuh , par_shape, par_scale); %second version
 
 % verification of gamma curve 
 sumIUHW = sum(IUHW*dt);
 
 figure(1)
-bar(T*dt,IUHW);
-
-%% testing gammy function
-
-T_test = 0:40;
-gammaIUH_test = gampdf(T_test ,7, 1);
-
-figure(2)
-bar(gammaIUH_test)
+%bar(T*dt,IUHW); %first version
+bar(t_iuh*dt,IUHW); %second version
 
 %% Channel IUH
 
-TC = linspace(0,100, 100/dt);
-sz = size(TC);
+% defining IUH time interval 
+cutoff = 70;
+%TC = linspace(0,100, 100/dt); %first version --> less precise 
+t_iuh=(dt:dt:cutoff); %second version 
+% sz = size(TC);
+
+%defining parameters of the probability density function 
 L = 10^4;
 D = 10^6;
 c = 0.3*3600;
-%length((L-c*TC).^2)
-%length(4*D*T)
-%length(sqrt(4*pi*D)*T.^(3/2))
 
-IUHC = L./sqrt(4*pi*D)*TC.^-(3/2).*exp(-((L-c*TC).^2)./(4*D*TC));
-IUHC(1)=0;
+% first version 
+%IUHC = L./sqrt(4*pi*D)*TC.^-(3/2).*exp(-((L-c*TC).^2)./(4*D*TC));
+%IUHC(1)=0;
+
+%second version 
+IUHC = L./sqrt(4*pi*D)*t_iuh.^-(3/2).*exp(-((L-c*t_iuh).^2)./(4*D*t_iuh));
 
 figure(3)
-bar(TC,IUHC);
+%bar(TC,IUHC);% first version
+bar(t_iuh,IUHC);% second version
 sumIUHC = sum(IUHC*dt);
 
 %% (3) IUHW figures 
@@ -63,63 +67,119 @@ sumIUHC = sum(IUHC*dt);
 figure(4)
 
 subplot(2,1,1);
-bar(T, IUHW, 'red');
+bar(t_iuh, IUHW, 'red');
 xlabel('step : ' + string(dt) +' h');
 ylabel('discretized probability distribution');
-xlim([0 150]);
-ylim([0 0.2]);
+xlim([0 50]);
+ylim([0 0.15]);
 title('IUHW');
-legend("Probability Density Curve");
+legend("Watershed Probability Density Curve");
 
 subplot(2,1,2);
-bar(TC, IUHC);
+bar(t_iuh, IUHC);
 xlabel('step : ' + string(dt) +' h');
 ylabel('discretized probability distribution');
-xlim([0 150]);
-ylim([0 0.2]);
+xlim([0 50]);
+ylim([0 0.15]);
 title('IUHC');
-legend("Probability Density Curve");
+legend("Channel Probability Density Curve");
 
 %% (4) Convolution 
 
 % Convolution over the effective precipitation falling on the watershed
-n = length(Jedt);
+[nrowsW,ncolsW] = size(Jedt);
 M = length(IUHW);
-N = n+M-1;
-DischargeW = zeros(1,N);
+NW = nrowsW+M-1;
+DischargeW = zeros(3,NW);
 
-for i = 1:n
-    DischargeW(i:i+M-1) = DischargeW(i:i+M-1)+ Je(i)*IUHW*dt;
+for l = 1:ncolsW
+    for i = 1:nrowsW
+        DischargeW(l,i:i+M-1) = DischargeW(l,i:i+M-1)+ Jedt(i,l)*IUHW*dt;
+    end 
 end 
-DischargeW;
+DischargeW = transpose(DischargeW);
 
-%% plotting the results to see the difference between the IUH and the system's response.
+%% plotting the results 
+xW = (dt:dt:NW*dt);
 figure
-subplot(2,1,1);
-bar(DischargeW)
-xlim([0 500])
-subplot(2,1,2);
-bar(IUHW);
-xlim([0 500]);
+for i = 1:3
+    subplot(3,1,i);
+    bar(xW,DischargeW(:, i))
+    title("event" + string(i))
+    xlim([0 40])
+    ylim([0 1.1])
+end 
 
 %% (5) IUHC convolution 
 
-n = length(Jedt);
+[nrowsC, ncolsC] = size(DischargeW);
 M = length(IUHC);
-N = n+M-1;
-DischargeC = zeros(1,N);
-
-for i = 1:n
-    DischargeC(i:i+M-1) = DischargeC(i:i+M-1)+ Je(i)*IUHC*dt;
+NC = nrowsC+M-1;
+DischargeC = zeros(3,NC);
+ 
+for l = 1:ncolsC % the inverse of what was done in the previous convolution
+    for i = 1:nrowsC % see comment above. This is due to the shape of the matrix defined 
+        DischargeC(l,i:i+M-1) = DischargeC(l,i:i+M-1)+ DischargeW(i,l)*IUHC*dt;
+    end 
 end 
-DischargeW;
+DischargeC = transpose(DischargeC);
 %% plotting the results to see the difference between the IUH and the system's response.
 figure
-subplot(2,1,1);
-bar(DischargeC)
+plot(DischargeW(:,1))
 xlim([0 500])
-ylim([0 0.6]);
-subplot(2,1,2);
-bar(IUHC);
+ylim([0 0.5]);
+hold on 
+plot(DischargeC(:,1));
 xlim([0 500]);
-ylim([0 0.6]);
+ylim([0 0.5]);
+hold off
+
+%% (6) figure containing everything 
+[spl_nbr, ev_nbr] = size(Jedt);
+
+figure
+for k = 1:ev_nbr
+    subplot(ev_nbr, 1, k)
+    xW = (dt:dt:NW*dt);
+    xC = (dt:dt:NC*dt);
+    bar(t_Jedt,Jedt(:,k));
+    hold on 
+    plot(xW, DischargeW(:,k));
+    hold on 
+    plot(xC-dt/10, DischargeC(:,k)); %-dt/10 is a way of visualizing the data. This way the histograms are not exactly superposed
+    hold off
+    xlim([0 35]);
+    ylim([0 max(Jedt(:,k)+0.2)]);
+    xlabel("time [h]")
+    ylabel("intensity [mm/h]")
+    if k == 1
+        legend("Effective Precipitation Intensity (Je)",  "Watershed Discharge Intensity (QW)",  "Channel Discharge Intensity (QC)")
+    end 
+    title(" Precipitation Event & Hydrological Response - " + string(k))
+end 
+
+%%
+
+figure
+xW = (dt:dt:NW*dt);
+xC = (dt:dt:NC*dt);
+bar(t_Jedt,Jedt(:,1));
+hold on
+plot(xW, DischargeW(:,1));
+hold on 
+plot(xC-dt/10, DischargeC(:,1)); %-dt/10 is a way of visualizing the data. This way the histograms are not exactly superposed
+hold off
+xlim([0 45]);
+ylim([0 0.8]);
+xlabel("time [h]")
+ylabel("intensity [mm/h]")
+legend("Effective Precipitation Intensity (Je)",  "Watershed Discharge Intensity (QW)",  "Channel Discharge Intensity (QC)")
+title("Precipitation & Hydrological Response")
+%% (7) Discharge peaks
+
+peakW = max(DischargeW);
+peakC = max(DischargeC);
+
+%% (8) saving variables 
+%uncomment the next line to save the desired results on your terminal
+%save('output_part2.mat','dt','IUHW');
